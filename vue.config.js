795a5c8450dd30c1841bfd8db092b980c87b5c86
lukas231424 <kreutzer.me@gmail.com>
2020-03-09 10:50:42 +0100

    updated text

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,38 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
-  lintOnSave: false,
-  pwa: {
-    name: "pwa-2019",
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "white",
-    // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "public/service-worker.js"
-      // ...other Workbox options...
+    lintOnSave: false,
+    pwa: {
+        name: "kreutzer",
+        themeColor: "#FFCE6D",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "white",
+        // configure the workbox plugin
+        workboxPluginMode: "InjectManifest",
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: "public/service-worker.js"
+        }
+    },
+    configureWebpack: {
+        plugins: [
+            new VuetifyLoaderPlugin()
+        ]
+    },
+    chainWebpack: config => {
+        // remove the prefetch plugin
+        config.plugins.delete("prefetch");
+        config.module
+            .rule("vue")
+            .use("vue-loader")
+            .loader("vue-loader")
+            .tap(options =>
+                Object.assign(options, {
+                    transformAssetUrls: {
+                        "v-img": ["src", "lazy-src"]
+                    }
+                })
+            );
     }
-  },
-  configureWebpack: {
-    plugins: [
-      new VuetifyLoaderPlugin()
-    ]
-  },
-  chainWebpack: config => {
-    // remove the prefetch plugin
-    config.plugins.delete("prefetch");
-    config.module
-      .rule("vue")
-      .use("vue-loader")
-      .loader("vue-loader")
-      .tap(options =>
-        Object.assign(options, {
-          transformAssetUrls: {
-            "v-img": ["src", "lazy-src"]
-          }
-        })
-      );
-  }
 };
